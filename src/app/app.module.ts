488c298ae0f7e7c2f15fbc4a8e12294f764fdad1
Chien DUONG <chienduong90@gmail.com>
2021-02-04 13:20:12 +0100

    RxJS

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,6 +4,7 @@ import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { AdminModule } from './admin/admin.module';
+import { HttpClientModule } from '@angular/common/http';
 
 
 @NgModule({
@@ -13,7 +14,8 @@ import { AdminModule } from './admin/admin.module';
   imports: [
     BrowserModule,
     AppRoutingModule,
-    AdminModule
+    AdminModule,
+    HttpClientModule
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ src/app/project.service.ts
@@ -0,0 +1,48 @@
+import { Injectable } from '@angular/core';
+import { HttpClient } from "@angular/common/http";
+import { Observable } from 'rxjs';
+import { Project } from './project';
+import { map, catchError } from 'rxjs/operators';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class ProjectService {
+
+  constructor(private httpClient : HttpClient) { }
+
+  getAllProjects() : Observable<Project[]>
+  {
+    return this.httpClient.get<Project[]>("/api/projects", { responseType: 'json' })
+    .pipe(map(
+      (data: Project[]) => {
+        for (let i=0; i < data.length; i++) {
+          data[i].teamSize = data[i].teamSize * 100;
+        }
+        return data;
+      }
+
+    ));
+  }
+
+  insertProject(newProject: Project) : Observable<Project> {
+    return this.httpClient.post<Project>("/api/post", newProject, { responseType: 'json' });
+  }
+
+  updateProject(existingProject: Project) : Observable<Project> {
+    return this.httpClient.put<Project>("api/put", existingProject, { responseType: 'json' });
+  }
+
+  deleteProject(ProjectID: number) : Observable<string>
+  {
+    return this.httpClient.delete<string>("/api/projects?ProjectID=" + ProjectID);
+  }
+
+  SearchProjects(searchBy: string, searchText: string): Observable<Project[]> {
+    return this.httpClient.get<Project[]>(
+      '/api/projects/search/' + searchBy + '/' + searchText,
+      { responseType: 'json' }
+    );
+  }
+
+}
